d6e3a591c6a69c932c508a3177aba7e064e21250
D0ne <d0ne@d0ne.com>
2025-06-30 08:47:45 +0100

    Initial Commit

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Button from "./Button";
+
+function Header() {
+    return(
+    <header>
+        <Button name="Home"/>
+        <Button name="Articles"/>
+        <Button name="About"/>
+    </header> 
+    )
+}
+
+export default Header;
---
+++ client/src/components/HeroStat.jsx
@@ -0,0 +1,7 @@
+
+export default function HeroStat(){
+    return (<div>
+
+    </div>
+    )
+}
