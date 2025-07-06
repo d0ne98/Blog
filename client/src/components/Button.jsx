0cd195baa1b5061007a9605e8d8878441a00cbd9
D0ne <d0ne@d0ne.com>
2025-07-06 13:49:13 +0100

    Add some changes

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,4 +1,18 @@
+import {useNavigate} from "react-router-dom"
 
 export default function Button(props) {
-    return <button onClick={props.handleClick}>{props.name}</button>
+
+    const navigate = useNavigate();
+    
+    function handleClick() {
+        if (props.handleClick) {
+            // If handleClick prop is provided, use it
+            props.handleClick();
+        } else if (props.url) {
+            // If url prop is provided, navigate to it
+            navigate(`${props.url}`);
+        }
+    }
+
+    return <button onClick={handleClick}>{props.name}</button>
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
     return(
     <header className="header">
         <nav className="nav">
-         <Button name="Home"/>
+         <Button name="Home" url="/"/>
          <Button name="Articles"/>
          <Button name="About"/>
         </nav>
